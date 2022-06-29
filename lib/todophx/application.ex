84e7875baf33f6e654a63ebe4938a9049ec34372
Arpit Kumar <arpit@nurture.farm>
2022-06-29 22:23:19 +0530

    addition of ets and endpoint start before window

--- lib/todophx/application.ex
+++ lib/todophx/application.ex
@@ -8,6 +8,16 @@ defmodule Todophx.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      # Start the Ecto repository
+      Todophx.Repo,
+      # Start the Telemetry supervisor
+      TodophxWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: Todophx.PubSub},
+      # Start the Endpoint (http/https)
+      TodophxWeb.Endpoint,
+      # Start a worker by calling: Todophx.Worker.start_link(arg)
+      # {Todophx.Worker, arg}
       {Desktop.Window,
        [
          app: :todophx,
@@ -16,18 +26,10 @@ defmodule Todophx.Application do
          title: "Todophx",
          icon: "icon.png",
          url: &TodophxWeb.Endpoint.url/0
-       ]},
-      # Start the Ecto repository
-      Todophx.Repo,
-      # Start the Telemetry supervisor
-      TodophxWeb.Telemetry,
-      # Start the PubSub system
-      {Phoenix.PubSub, name: Todophx.PubSub},
-      # Start the Endpoint (http/https)
-      TodophxWeb.Endpoint
-      # Start a worker by calling: Todophx.Worker.start_link(arg)
-      # {Todophx.Worker, arg}
+       ]}
     ]
+
+    :session = :ets.new(:session, [:named_table, :public, read_concurrency: true])
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
